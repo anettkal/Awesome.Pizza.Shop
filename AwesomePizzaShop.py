168e6f5eae437a05809109c6b6123d0624624244
anettkal <anett.kalamar@gmail.com>
2020-01-10 10:30:37 +0100

    added Pizza class, input for pizzas

--- AwesomePizzaShop.py
+++ AwesomePizzaShop.py
@@ -15,14 +15,27 @@ class PizzaShop:
             if option == '1':
                 print(self.name + self.address + self.opening_time)
             elif option == '2':
-                print("")
+                print("Do you want to view pizza selection or make own AWESOME pizza?")
+                select = input("Press 1, for pizza selection, press 2, to make own Awesome pizza.")
             elif option == '3':
                 print("Goodbye! See you next time.")
+                break
             else:
                 self.choose_next_option()
 
 
+class Pizza:
+    def __init__(self, name, list_of_ingredients, price):
+        self.name = name
+        self.price = price
+        self.list_of_ingredients = list_of_ingredients
+
+    def get_price(self):
+        return self.price
+
+
 if __name__ == '__main__':
-    ps = PizzaShop("Awesome PizzaShop", "Corso di Porta Romana 83, 20122 Milano",
+    ps = PizzaShop("Awesome PizzaShop, ", "Corso di Porta Romana 83, 20122 Milano, ",
                    "Monday - Saturday ~ (11:00 - 23:00), Sunday ~ (closed)")
     ps.choose_next_option()
+
